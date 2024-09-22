3eabe0786b8a46f991999b2ab8b23a548ae4d15f
rrehus <rrehus98@gmail.com>
2024-09-22 09:59:40 -0400

    Final tasks

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,9 +6,13 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }  
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  
-    response = json.loads(response.text)
-    emotions = response["emotionPredictions"][0]["emotion"]
-    max_emotion_score = max(emotions.values())
-    emotions["dominant_emotion"] = [key for key,value in emotions.items() if value==max_emotion_score][0]
+    status = response.status_code
+    if status == 200:
+      response = json.loads(response.text)
+      emotions = response["emotionPredictions"][0]["emotion"]
+      max_emotion_score = max(emotions.values())
+      emotions["dominant_emotion"] = [key for key,value in emotions.items() if value==max_emotion_score][0]
+    elif status == 400:
+        emotions = { 'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None,  \
+        "dominant_emotion":None}
     return emotions
-
--- server.py
+++ server.py
@@ -1,24 +1,31 @@
+"""Web Deployment of Emotion Detector App with Error Handling"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+
 @app.route("/emotionDetector")
 def sent_detector():
+    """Display response from Emotion Detector"""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
+    if response["dominant_emotion"] is None:
+        return "<b>Invalid text! Please try again!</b>"
+
     # Return a formatted string with the sentiment label and score
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {},  \
-    'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(  \
-        response["anger"],response["disgust"],response["fear"],response["joy"],  \
-        response["sadness"], response["dominant_emotion"])
+    return f"<div>For the given statement, the system response is 'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': \
+    {response['sadness']}. The dominant emotion is <b>{response['dominant_emotion']}</b></div>"
 
 @app.route("/")
 def render_index_page():
+    """Index page rendering"""
     return render_template('index.html')
 
 if __name__ == "__main__":
